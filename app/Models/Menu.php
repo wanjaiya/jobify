354cb4f59549d4c4ca9bb96049a20dbd3ab197a8
Wanjaiya <kangethewanjaiya@gmail.com>
2026-01-15 15:39:37 +0300

    menu

--- app/Models/Menu.php
+++ app/Models/Menu.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 use App\Models\Traits\HasPosition;
 use App\Models\Traits\HasSlug;
 use App\Models\Traits\HasStatus;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Menu extends Model
 {
@@ -15,7 +16,7 @@ class Menu extends Model
 
     protected $fillable = ['name', 'slug', 'status', 'published_at', 'position', 'menu_type_id'];
 
-    public function types(){
+    public function types(): BelongsTo{
         return $this->belongsTo(MenuType::class, 'menu_type_id', 'id');
     }
 }
