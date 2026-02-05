13ce212a5eae21f7e048f4cf0684e7628be98937
wanjaiya <kangethewanjaiya@gmail.com>
2026-02-05 22:31:04 +0300

    Icon Columns

--- app/Filament/Resources/Companies/Tables/CompaniesTable.php
+++ app/Filament/Resources/Companies/Tables/CompaniesTable.php
@@ -8,7 +8,7 @@ use Filament\Actions\EditAction;
 use Filament\Tables\Table;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Columns\ImageColumn;
-
+use Filament\Tables\Columns\IconColumn;
 
 class CompaniesTable
 {
@@ -17,6 +17,13 @@ class CompaniesTable
         return $table
             ->columns([
                 //
+                IconColumn::make('published')
+                    ->label('Status')
+                    ->trueIcon('heroicon-o-check-circle')
+                    ->falseIcon('heroicon-o-x-circle')
+                    ->trueColor('success')
+                    ->falseColor('danger')
+                    ->boolean(),
 
                 ImageColumn::make('logo')->disk('public')
                     ->label('Logo'),
